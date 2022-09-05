6a1650441449d43e0fd259bf008cb9ac723224a5
AgogFox <bopawat@gmail.com>
2022-09-05 13:44:04 +0700

    finish prob2 sol1

---
+++ 2/solution-1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main ()
+{
+    float weight, height;
+
+    printf("Enter your weight : ");
+    scanf("%f", &weight);
+    printf("Enter your height : ");
+    scanf("%f", &height);
+    weight = weight / (height * height);
+    if (weight < 18.5)
+    {
+    printf("Underweight");
+    }
+    else if (weight >= 18.5 && weight <=24.9)
+    {
+    printf("Normal");
+    }
+    else if (weight >= 25.0 && weight <= 29.9)
+    {
+    printf("Taller than normal");
+    }
+    else
+    {
+    printf("Overweight");
+    } 
+}
