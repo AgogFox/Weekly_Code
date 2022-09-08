1490099d019299238e6eb9f71d40b1d4d7e233ac
AgogFox <bopawat@gmail.com>
2022-09-08 22:18:08 +0700

    chg prob3, fin prob3sol1

---
+++ 3/solution-1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main ()
+{
+    int hight = 0;
+    scanf("%d", &hight);
+
+    for (int i = 1; i <= hight; i++)
+    {
+        for (int k = 1; k <= hight - i; k++)
+        {
+            printf(" ");
+        }
+        
+        for (int k = 1; k <= i*2 - 1; k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
